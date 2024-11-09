0b9de816ab87706724a89013bd15046405de67b1
iola1999 <iola1999@foxmail.com>
2024-11-10 01:21:26 +0800

    chore: disable scale

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -9,6 +9,12 @@ export const GlobalStyles = ({ theme }: { theme: Theme }) => (
         padding: 0;
         box-sizing: border-box;
         -webkit-tap-highlight-color: transparent;
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none; /* Non-prefixed version */
       }
 
       body {
